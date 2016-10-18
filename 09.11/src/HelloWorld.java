a0e4c7269e1558a1d2eb1d96d5540cbcdd6e5b68
soft <soft@soft-PC>
2016-10-18 17:20:08 +0900

    09.11 git start

---
+++ 09.11/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("tired..........");
+		System.out.println("World is hello");
+	}
+}
